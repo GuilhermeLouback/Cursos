8aea768e69db48e3784efc58238aa4841b20545d
GuilhermeLouback <guilherme_lou@hotmail.com>
2023-05-04 21:55:31 -0300

    first commit

---
+++ Curso de Python/Controle de Fluxo/Multiplos operadores de comparação.py
@@ -0,0 +1,16 @@
+#Multiplos operadores de comparação 
+
+
+valor = 10
+
+if 20 <= valor < 40:
+    print('Produto Aceito')
+
+
+# '''if valor >= 20 and valor < 40: 
+#     print('Produto Aceito')'''
+
+
+
+else:
+    print('Produto não Aceito')
